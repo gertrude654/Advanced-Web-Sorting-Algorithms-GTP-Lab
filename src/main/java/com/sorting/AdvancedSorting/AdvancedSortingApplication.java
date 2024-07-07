3e79c5c54c1a9e06a2e5e903e31bb98c598920b3
gertrude654 <gertrudegefi11@gmail.com>
2024-07-08 00:17:44 +0200

    Jar added

--- src/main/java/com/sorting/AdvancedSorting/AdvancedSortingApplication.java
+++ src/main/java/com/sorting/AdvancedSorting/AdvancedSortingApplication.java
@@ -2,10 +2,16 @@ package com.sorting.AdvancedSorting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AdvancedSortingApplication {
+public class AdvancedSortingApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AdvancedSortingApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(AdvancedSortingApplication.class, args);
 	}
